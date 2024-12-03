836ecaff23161ecec6e101e32d476a14c12f8c2a
ilhammmaulana <ilhammmaulana688@gmail.com>
2024-12-03 23:38:46 +0700

    add: article page

--- resources/views/pages/guest/articles/index.blade.php
+++ resources/views/pages/guest/articles/index.blade.php
@@ -3,27 +3,25 @@
         <h1 class="text-3xl font-bold text-center mb-6">Artikel</h1>
         <hr>
         <!-- Dropdown Kategori -->
-        <div class="flex justify-end mb-4 mt-3">
+        <div class="flex justify-start mb-4 mt-3 md:mx-40">
             <div class="relative">
-                <select
-                    class="block w-full px-4 py-2 pr-8 bg-white border border-gray-300 rounded shadow appearance-none focus:outline-none focus:ring-2 focus:ring-blue-500">
-                    <option>Kategori</option>
+                <select class="select select-ghost w-72">
+                    <option class="font-bold">
+                        Kategori
+                    </option>
                     <!-- Tambahkan kategori lain jika ada -->
                     <option>Desain</option>
                     <option>Teknologi</option>
                     <option>UI/UX</option>
                 </select>
-                <span class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
-                    ▼
-                </span>
             </div>
         </div>
 
         <!-- Grid Artikel -->
-        <div class="bg-white py-8 px-8 rounded-md">
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div class="bg-white py-8 px-8 rounded-md md:mx-20">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
                 @foreach ($articles as $article)
-                    <a class="card mx-auto bg-base-100 w-full" href="{{ route('articles.show', $article->slug) }}">
+                    <a class="card mx-auto bg-base-100 w-80" href="{{ route('articles.show', $article->slug) }}">
                         <figure class="w-full h-64 overflow-hidden rounded-lg">
                             <img src="{{ asset('storage/' . $article->image) }}" alt="{{ $article->title }}"
                                 class="w-full h-full object-cover" />
